bd939eafb49ca84cb1e8e54103e35830a3492f2b
Victor Vuelma <victorvuelma@outlook.com>
2021-10-23 19:20:49 -0300

    feat(details): improvements on details page

--- pages/Detail/index.tsx
+++ pages/Detail/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 import { useRoute } from "@react-navigation/core";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
+import dayjs from "dayjs";
+
 import { Film } from "../../model/Filme";
-import { Container } from "./style";
-import { ScrollView } from "react-native-gesture-handler";
+import {
+  BackButton,
+  Container,
+  Description,
+  LabelButton,
+  Poster,
+  RatingBox,
+  RatingIcon,
+  RatingText,
+  ReleaseDate,
+  Title,
+} from "./style";
 
 interface NavigationProps {
   navigation: NativeStackNavigationProp<{
@@ -23,7 +38,38 @@ export default function DetailPage({ navigation }: NavigationProps) {
 
   return (
     <ScrollView>
-      <Container></Container>
+      <Container>
+        <SafeAreaView>
+          <Poster
+            source={{
+              uri:
+                "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/" +
+                film.backdrop_path,
+            }}
+          />
+
+          <Title>{film.title}</Title>
+
+          <RatingBox>
+            <RatingIcon name="star" />
+            <RatingText>
+              {film.vote_average.toLocaleString("pt-BR", {
+                minimumFractionDigits: 1,
+              })}
+            </RatingText>
+          </RatingBox>
+
+          <Description>{film.overview}</Description>
+
+          <ReleaseDate>
+            Data de lançamento: {dayjs(film.release_date).format("DD/MM/YYYY")}
+          </ReleaseDate>
+
+          <BackButton onPress={() => handleBack()}>
+            <LabelButton>Voltar</LabelButton>
+          </BackButton>
+        </SafeAreaView>
+      </Container>
     </ScrollView>
   );
 }
--- pages/Detail/style.ts
+++ pages/Detail/style.ts
@@ -1,100 +1,66 @@
-import { FlatList } from "react-native";
-
 import styled from "styled-components/native";
 import { MaterialIcons } from "@expo/vector-icons";
-import { RectButton } from "react-native-gesture-handler";
-
-import { Film } from "../../model/Filme";
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.color.background};
 `;
 
-export const PosterHeader = styled.Image`
-  width: 500px;
-  height: 250px;
-  opacity: 0.7;
+export const Poster = styled.Image`
+  width: 480px;
+  height: 300px;
+  opacity: 0.8;
 `;
 
-export const TitleHeader = styled.Text`
+export const Title = styled.Text`
   color: #fff;
-  font-size: 36px;
+  font-size: 28px;
   text-align: center;
-  text-transform: uppercase;
-  font-weight: 500;
-  letter-spacing: 4px;
-  top: -50px;
+  font-weight: 700;
+  margin: 16px 0;
 `;
 
-export const ButtonHeader = styled.TouchableOpacity`
+export const RatingBox = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const RatingIcon = styled(MaterialIcons)`
+  color: gold;
+  font-size: 24px;
+`;
+
+export const RatingText = styled.Text`
+  text-align: center;
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 24px;
+  margin-left: 4px;
+`;
+
+export const Description = styled.Text`
+  color: ${({ theme }) => theme.color.text};
+  font-size: 20px;
+  margin: 8px 16px;
+`;
+
+export const ReleaseDate = styled.Text`
+  color: ${({ theme }) => theme.color.text};
+  font-size: 16px;
+  margin: 8px 16px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
   align-items: center;
   background-color: ${({ theme }) => theme.color.primary};
-  border-radius: 5px;
+  border-radius: 8px;
   padding: 8px 32px;
-  top: -40px;
+  margin: 32px 16px;
 `;
 
 export const LabelButton = styled.Text`
   color: ${({ theme }) => theme.color.text};
   font-size: 18px;
   font-family: ${({ theme }) => theme.font.regular};
-`;
-
-export const Header = styled.View`
-  align-items: center;
-`;
-
-export const SectionTitle = styled.Text`
-  color: #fff;
-  font-size: 16px;
-  border-color: ${({ theme }) => theme.color.primary};
-  border-left-width: 4px;
-  padding-left: 8px;
-  margin-left: 8px;
-`;
-
-export const Section = styled.View`
-  margin-top: 4px;
-  margin-bottom: 24px;
-`;
-
-export const HorizontalList = styled(
-  FlatList as new () => FlatList<Film>
-).attrs({
-  horizontal: true,
-  showsHorizontalScrollIndicator: false,
-})``;
-
-export const CardFilm = styled(RectButton)`
-  width: 100px;
-  margin: 8px;
-`;
-
-export const Poster = styled.Image`
-  height: 150px;
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-`;
-
-export const FilmRating = styled.View`
-  background-color: rgba(0, 0, 0, 0.5);
-  border-bottom-left-radius: 8px;
-  border-bottom-right-radius: 8px;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  padding: 2px;
-`;
-
-export const RatingIcon = styled(MaterialIcons)`
-  color: gold;
-  font-size: 16px;
-`;
-
-export const Rating = styled.Text`
-  text-align: center;
-  color: rgba(255, 255, 255, 0.8);
-  font-size: 18px;
-  margin-left: 4px;
 `;
